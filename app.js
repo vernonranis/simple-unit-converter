40e4d66948d13e24eabb76aefe499877ad00b0f7
vernonranis <vernonranis@gmail.com>
2021-08-06 18:46:00 +0800

    partially finished HTML and CSS

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input-option").selectedIndex;
+const inputTemp = document.querySelectorAll("option")[input].value;
+
+const output = document.querySelector(".output-option").selectedIndex;
+const outputTemp = document.querySelectorAll("option")[output].value;
+
+// console.log(inputValue + outputValue);
+
