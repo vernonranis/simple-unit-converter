44efec86bc72db52c9bc7aa25ed6c051ef2c4892
vernonranis <vernonranis@gmail.com>
2021-08-08 11:28:03 +0800

    finished the switch value with compute

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const temp = ["fahrenheit",  "celsius", "kelvin", "rankine", "réaumur", "delisle", "rømer", "newton"];
+
 const inputValue = document.querySelector('.input-field');
+const outputValue = document.querySelector('.output-field');
 
 document.querySelector(".btn").addEventListener("click", () =>{
   const inputType = document.querySelector(".input-option").value;
   const outputType = document.querySelector(".output-option").value;
-
+  
   if (document.querySelector(".input-option option").className === "input"){
     document.querySelector(".input-option").value = outputType;
     document.querySelector(".output-option").value = inputType;
     updateSelectedOption();
+    compute();
   }
 });
 
@@ -17,15 +21,15 @@ function updateSelectedOption() {
   const currentOutputOption = document.querySelector(".output-option").value;
   const newInputOption = document.querySelectorAll(".input-option option");
   const newOutputOption = document.querySelectorAll(".output-option option");
-
+  
   for (let index = 0; index < newInputOption.length; index++) {
     document.querySelectorAll(".input-option option")[index].removeAttribute("id");
   }
-
+  
   for (let index = 0; index < newOutputOption.length; index++) {
     document.querySelectorAll(".output-option option")[index].removeAttribute("id");
   }
-
+  
   for (let i = 0; i < newInputOption.length; i++) {
     const newInputOptionLoop = newInputOption[i];
     if (currentInputOption === newInputOptionLoop.value) {
@@ -33,6 +37,7 @@ function updateSelectedOption() {
       break
     }
   }
+  
   for (let i = 0; i < newOutputOption.length; i++) {
     const newOutputOptionLoop = newOutputOption[i];
     if (currentOutputOption === newOutputOptionLoop.value) {
@@ -40,4 +45,30 @@ function updateSelectedOption() {
       break
     }
   }
+  compute();
 }
+
+function compute() {
+  const inputID = document.querySelector("#input-selected").value;
+  const outputID = document.querySelector("#output-selected").value;
+
+  switch (true) {
+    case inputID === temp[0] && outputID === temp[1]:
+      outputValue.value = ((5/9)*(parseFloat(inputValue.value)-32)).toFixed(2);
+      console.log("case 0");
+    break;
+    
+    case inputID === temp[1] && outputID === temp[0]:
+      outputValue.value = ((parseFloat(inputValue.value)*(9/5)+32)).toFixed(2);
+      console.log("case 1");
+    break;
+  
+    default:
+      console.log("not working");
+      outputValue.value = (" ");
+      break;
+  }
+}
+inputValue.addEventListener("input", () =>{
+  compute();
+});
