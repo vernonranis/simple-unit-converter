3c6e366029f057b582987124cb97d6dd5d849944
vernonranis <vernonranis@gmail.com>
2021-08-08 08:47:08 +0800

    completed the ID tagging for input and output

--- app.js
+++ app.js
@@ -14,16 +14,29 @@ document.querySelector(".btn").addEventListener("click", () =>{
 
 function updateSelectedOption() {
   const currentInputOption = document.querySelector(".input-option").value;
+  const currentOutputOption = document.querySelector(".output-option").value;
   const newInputOption = document.querySelectorAll(".input-option option");
+  const newOutputOption = document.querySelectorAll(".output-option option");
 
   for (let index = 0; index < newInputOption.length; index++) {
     document.querySelectorAll(".input-option option")[index].removeAttribute("id");
+  }
+
+  for (let index = 0; index < newOutputOption.length; index++) {
+    document.querySelectorAll(".output-option option")[index].removeAttribute("id");
   }
 
   for (let i = 0; i < newInputOption.length; i++) {
     const newInputOptionLoop = newInputOption[i];
     if (currentInputOption === newInputOptionLoop.value) {
       document.querySelectorAll(".input-option option")[i].setAttribute("id", "input-selected");
+      break
+    }
+  }
+  for (let i = 0; i < newOutputOption.length; i++) {
+    const newOutputOptionLoop = newOutputOption[i];
+    if (currentOutputOption === newOutputOptionLoop.value) {
+      document.querySelectorAll(".output-option option")[i].setAttribute("id", "output-selected");
       break
     }
   }
